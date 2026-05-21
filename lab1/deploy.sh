98b47f8e2d3bd7a5f1fa41162c2efdf88bd74f7a
Yuliia <julia.kostandi@gmail.com>
2026-05-21 23:30:49 +0300

    Added cd pipeline and deploy script

---
+++ lab1/deploy.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+IMAGE_NAME=$1
+
+echo "Починаємо розгортання образу: $IMAGE_NAME"
+
+sudo apt-get update
+sudo apt-get install -y docker.io nginx
+
+cat <<EOF | sudo tee /etc/systemd/system/mywebapp.service
+[Unit]
+Description=My Web App Docker Container
+Requires=docker.service
+After=docker.service
+
+[Service]
+Restart=always
+ExecStartPre=-/usr/bin/docker rm -f mywebapp-container
+ExecStart=/usr/bin/docker run --name mywebapp-container -p 3000:3000 \$IMAGE_NAME
+ExecStop=/usr/bin/docker stop mywebapp-container
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+sudo systemctl daemon-reload
+sudo systemctl enable mywebapp.service
+sudo systemctl restart mywebapp.service
+
+cat <<EOF | sudo tee /etc/nginx/sites-available/mywebapp
+server {
+    listen 80;
+    server_name _;
+
+    location / {
+        proxy_pass http://127.0.0.1:3000;
+        proxy_set_header Host \$host;
+        proxy_set_header X-Real-IP \$remote_addr;
+    }
+}
+EOF
+
+sudo ln -sf /etc/nginx/sites-available/mywebapp /etc/nginx/sites-enabled/
+sudo rm -f /etc/nginx/sites-enabled/default
+sudo systemctl restart nginx
+
+echo "Розгортання успішно завершено"
